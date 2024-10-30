cee624b5ff853b2d8d834f90b648789f115543ee
yixuan173 <testyixuan173@gmail.com>
2024-10-30 12:41:47 +0800

    feat: 基本頁面樣式配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import './App.css'
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 
 import Home from './pages/Home';
 import Search from './pages/Search';
 import ToWatchList from './pages/ToWatchList';
 
+const Layout = styled.div`
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1rem;
+  gap: 4rem;
+`;
+
+const Content = styled.div`
+`;
+
+const Icon = styled.div`
+  color: #fff;
+`;
+
 function App() {
   return (
-    <>
-      <h1>Simple Movie Website</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-            <li>
-              <Link to="/toWatchList">To Watch List</Link>
-            </li>
-          </ul>
-        </nav>
+    <Layout>
+      <Header>
+        <Link to="/">        
+          <Icon>
+            <ion-icon name="home" size="large"></ion-icon>
+          </Icon>
+        </Link>
+        <Link to="/search">        
+          <Icon>
+            <ion-icon name="search" size="large"></ion-icon>
+          </Icon>
+        </Link>
+        <Link to="/toWatchList">        
+          <Icon>
+          <ion-icon name="list" size="large"></ion-icon>
+          </Icon>
+        </Link>
+      </Header>
+      <Content>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/search' element={<Search />} />
           <Route path='/toWatchList' element={<ToWatchList />} />
         </Routes>
-    </>
+      </Content>
+    </Layout>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.css'
+import './main.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
