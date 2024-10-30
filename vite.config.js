9621344aa3da199ce672366b1e6f758871e93b03
yixuan173 <testyixuan173@gmail.com>
2024-10-30 10:29:14 +0800

    feat: add .env file setting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/movie-website",
+  define: {
+    'process.env': process.env
+  }
 })
