5851086fb7861aa9ce20e3c4a4a54d94b8731579
yixuan173 <testyixuan173@gmail.com>
2024-11-01 12:04:46 +0800

    feat: add GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import './main.css'
 import App from './App.jsx'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background-color: #000;
+  }
+`;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle />
     <Router basename="/movie-website">
       <App />
     </Router>
