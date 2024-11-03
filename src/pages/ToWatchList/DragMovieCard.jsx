929ff2c5beaf0a537ac8d8391e00736f8066f554
yixuan173 <testyixuan173@gmail.com>
2024-11-04 01:36:19 +0800

    feat: 新增拖曳元件

---
+++ src/pages/ToWatchList/DragMovieCard.jsx
@@ -0,0 +1,41 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+import { MovieCardWrapper } from "./style";
+import MovieCard from "../../components/MovieCard";
+
+
+const DragMovieCard = ({ movie, movieIndex }) => {
+    const {attributes, listeners, transform, transition, setNodeRef, isDragging} = useSortable({
+        id: movie.id, 
+        data: {
+            index: movieIndex,
+        },
+        transition: null,
+    })
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        zIndex: isDragging ? 2: 'auto'
+    }
+
+    return (
+        <MovieCardWrapper 
+            ref={setNodeRef} 
+            {...attributes} 
+            {...listeners}
+            style={style}
+            id={movie.id}
+        >
+            <MovieCard 
+                movieInfo={movie} 
+                isShowTitle
+                isShowToWatchBtn
+                isEnableOverlay
+            />
+        </MovieCardWrapper>
+    )
+}
+
+export default DragMovieCard;
---
+++ src/pages/ToWatchList/DragMovieList.jsx
@@ -0,0 +1,31 @@
+import { closestCorners, DndContext } from "@dnd-kit/core"
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+
+import { MoviesContainer } from "./style";
+import DragMovieCard from "./DragMovieCard";
+import useToWatchListStore from "../../stores/useToWatchListStore"
+
+
+const DragMovieList = ({ movieList }) => {
+    const sortToWatchList = useToWatchListStore((state) => state.sortToWatchList);
+
+    const handleDragEnd = (e) => {
+        const { active, over } = e;
+        if (active.id === over.id) return;
+        sortToWatchList(arrayMove(movieList, active.data.current.index, over.data.current.index));
+    }
+
+    return (
+        <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+            <MoviesContainer>  
+                <SortableContext items={movieList}>
+                    {movieList?.length > 0 ? movieList.map((movie, index) => (
+                        <DragMovieCard key={movie.id} movie={movie} movieIndex={index} />
+                    )) : null}
+                </SortableContext>
+            </MoviesContainer>
+        </DndContext>
+    )
+}
+
+export default DragMovieList;
